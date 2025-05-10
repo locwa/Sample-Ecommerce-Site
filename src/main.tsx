156cacf37399ad259e6cb6561aebe3f62448ea63
locwa <94048557+locwa@users.noreply.github.com>
2025-05-10 11:14:30 +0800

    Adapted main.tsx to react-router.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import Products from './Products.tsx'
+import { BrowserRouter, Routes, Route} from "react-router";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+      <BrowserRouter>
+          <Routes>
+              <Route path='/' element={<Products />} />
+              <Route path='/:category' element={<Products />} />
+          </Routes>
+      </BrowserRouter>
+  </StrictMode>
 )
