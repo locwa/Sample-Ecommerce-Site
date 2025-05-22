8e923369f10f600f3041471f726fb8197ce0aded
locwa <94048557+locwa@users.noreply.github.com.>
2025-05-22 21:10:04 +0800

    Added Product model with Database class that will connect to the MySQL Database

---
+++ backend/src/Models/ProductModel.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+
+use PDO;
+use Dotenv\Dotenv;
+
+class Database{
+    protected function connect(){
+        $dotenv = Dotenv::createImmutable(__DIR__);
+        $dotenv->load();
+        $user = $_ENV['DB_USER'];
+        $pwd = $_ENV['DB_PASSWORD'];
+        $host = $_ENV['DB_HOST'];
+        $dbname = $_ENV['DB_NAME'];
+
+        $dsn = "mysql:host=". $host .";dbname=". $dbname;
+
+
+        // connection to database
+        try{
+            $conn = new PDO($dsn, $user, $pwd);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch (PDOException $e){
+            echo "connection failed:". $e->getMessage();
+       }
+    }
+}
+
+class ProductModel{
+
+}
