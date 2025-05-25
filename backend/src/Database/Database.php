93214021b3d942a8fbcf4ff709a827be66418be3
locwa <94048557+locwa@users.noreply.github.com.>
2025-05-25 12:43:00 +0800

    Moved Database class to its own file

---
+++ backend/src/Database/Database.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Database;
+
+use PDO;
+use Dotenv\Dotenv;
+
+/**
+ * A class for connecting to the database
+ *
+ * The Database class contains the connect() method, which is the method to connect to the database
+ */
+class Database extends PDO {
+    /**
+     * A method to connect to the database
+     *
+     * This method connects to the database, and provides PDO methods that can be used to do SQL queries
+     */
+    public final function __construct(){
+        $dotenv = Dotenv::createImmutable(__DIR__, '/../../.env');
+        $dotenv->safeLoad();
+
+        $user = $_ENV['DB_USER'];
+        $pwd = $_ENV['DB_PASSWORD'];
+        $host = $_ENV['DB_HOST'];
+        $dbname = $_ENV['DB_NAME'];
+
+        $dsn = "mysql:host=". $host .";dbname=". $dbname;
+
+        // connection to database
+        try{
+            parent::__construct($dsn, $user, $pwd);
+            $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch (PDOException $e){
+            echo "connection failed:". $e->getMessage();
+        }
+    }
+}
