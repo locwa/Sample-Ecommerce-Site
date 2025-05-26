5261139c64a2d12a2547a5f9b24e4e3c8bccb7e8
locwa <94048557+locwa@users.noreply.github.com.>
2025-05-26 19:43:46 +0800

    Added configuration code for Apollo Client

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,14 +3,22 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Products from './Products.tsx'
 import { BrowserRouter, Routes, Route} from "react-router";
+import { ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
+
+const client = new ApolloClient({
+    uri: 'http://localhost:8082/graphql',
+    cache: new InMemoryCache()
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <BrowserRouter>
-          <Routes>
-              <Route path='/' element={<Products />} />
-              <Route path='/:category' element={<Products />} />
-          </Routes>
-      </BrowserRouter>
+      <ApolloProvider client={client}>
+          <BrowserRouter>
+              <Routes>
+                  <Route path='/' element={<Products />} />
+                  <Route path='/:category' element={<Products />} />
+              </Routes>
+          </BrowserRouter>
+      </ApolloProvider>
   </StrictMode>
 )
