d78ea3a441ab6a0d08ad103e364bed01b50adc7f
locwa <94048557+locwa@users.noreply.github.com.>
2025-05-26 20:40:09 +0800

    Made react render product cards based on GraphQL query and result

--- frontend/src/Products.tsx
+++ frontend/src/Products.tsx
@@ -1,19 +1,71 @@
 import "./App.css"
 import Navbar from "./Navbar.tsx"
 import { useParams } from "react-router";
+import {gql, useQuery} from "@apollo/client";
 
-function ProductCard(){
-    return(
-        <section className='my-14'>
-            <div className='h-80 w-72 bg-black'></div>
-            <h4 className='my-2 text-xl font-light'>Product 1</h4>
-            <p className='text-lg'>$54.99</p>
-        </section>
-    )
+type Currency = {
+    symbol: string;
+}
+
+type Price = {
+    amount: number;
+    currency: Currency;
+}
+
+type Product = {
+    id: string;
+    name: string;
+    inStock: boolean;
+    gallery: string[];
+    prices: Price;
+};
+
+type ProductsData = {
+    products: Product[];
+};
+
+function ProductCards(){
+
+    const GET_PRODUCTS = gql`
+        query GetProducts {
+            products{
+               id
+               name
+               inStock
+               gallery
+               prices{
+                amount
+                currency{
+                    symbol
+                }
+               }
+            }
+        }
+    `;
+
+    const { data, error, loading } = useQuery<ProductsData>(GET_PRODUCTS);
+
+    if (loading) return <p className="text-xl my-10">Loading...</p>;
+    if (error) return <p>Oops. It seems there is an error loading all products</p>;
+
+    return (
+        <div className="flex flex-wrap gap-12">
+            {data?.products.map(({ id, name, gallery, prices, inStock }) => (
+                <section className="my-14" key={id}>
+                    <div className="h-80 w-72 relative">
+                        <img src={gallery[0]} alt="product image" className={inStock ? "h-80 w-72 object-cover" : "h-80 w-72 object-cover grayscale brightness-80"}/>
+                        {!inStock && <p className="absolute top-40 text-center w-full h-full text-[#4f4f4f] text-lg">OUT OF STOCK</p>}
+                    </div>
+                    <h4 className="my-2 text-xl font-light">{name}</h4>
+                    <p className='text-lg'>{ prices['currency']['symbol'] + prices['amount'] }</p>
+                </section>
+            ))}
+        </div>
+    );
 }
 
 export default function Products(){
-    let { category } = useParams();
+    const { category } = useParams();
     let content = (
         <></>
     );
@@ -43,16 +95,7 @@ export default function Products(){
             <Navbar />
             <main className='p-12'>
                 {content}
-                <div className='flex flex-wrap gap-4'>
-                    <ProductCard/>
-                    <ProductCard/>
-                    <ProductCard/>
-                    <ProductCard/>
-                    <ProductCard/>
-                    <ProductCard/>
-                    <ProductCard/>
-                    <ProductCard/>
-                </div>
+                    <ProductCards/>
             </main>
         </>
     )
