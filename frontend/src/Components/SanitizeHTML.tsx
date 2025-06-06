c89f56af5dda4abd833450326ab562f0c86efd13
locwa <94048557+locwa@users.noreply.github.com>
2025-06-07 00:10:02 +0800

    Installed and used html-react-parser to parse HTML from database

--- frontend/src/Components/SanitizeHTML.tsx
+++ frontend/src/Components/SanitizeHTML.tsx
@@ -1,15 +1,17 @@
 import DOMPurify from "dompurify";
+import parse from "html-react-parser";
 import type {SanitizeHtmlProps} from "../Types/SanitizeHtmlProps";
 
 export default function SanitizeHTML({html} : SanitizeHtmlProps) {
     const rawHtml = html.replaceAll("\\n", "<br>")
     const sanitizedHtml = DOMPurify.sanitize(rawHtml);
+    const output = parse(sanitizedHtml);
 
     return (
         <div
             className="prose"
-            dangerouslySetInnerHTML={{__html: sanitizedHtml}}
-        />
+        >
+            {output}
+        </div>
     )
-
 }
