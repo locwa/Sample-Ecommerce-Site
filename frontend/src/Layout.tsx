4ac49d74f7f39c6c756e5689122c98d7e95afae9
locwa <94048557+locwa@users.noreply.github.com>
2025-06-06 14:34:49 +0800

    Put navbar and cart overlay in header

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -10,27 +10,29 @@ export default function Layout(
     return(
         <>
             <title>OmniShop</title>
-            <Navbar />
-            {isCartOpen && (
-                <>
-                    <div
-                        className="fixed top-15 left-0 w-full h-full bg-[#00000055] z-20"
-                        onClick={toggleCart}
-                    />
+            <header>
+                <Navbar />
+                {isCartOpen && (
+                        <>
+                            <div
+                                className="fixed top-15 left-0 w-full h-full bg-[#00000055] z-20"
+                                onClick={toggleCart}
+                            />
 
-                    <div className="fixed right-15 bg-white h-8/12 w-96 py-6 px-4 z-40">
-                        <div className="flex my-6">
-                            <h5 className="mr-2">My Bag,</h5>
-                            <p>{` ${cartCount} items`}</p>
-                        </div>
-                        <div className="text-center">
-                            {cartCount ? <CartItems/> : "no items"}
-                        </div>
-                    </div>
-                </>
+                            <div className="fixed right-15 bg-white h-8/12 w-96 py-6 px-4 z-40">
+                                <div className="flex my-6">
+                                    <h5 className="mr-2">My Bag,</h5>
+                                    <p>{` ${cartCount} items`}</p>
+                                </div>
+                                <div className="text-center">
+                                    {cartCount ? <CartItems/> : "no items"}
+                                </div>
+                            </div>
+                        </>
 
-            )
-            }
+                    )
+                }
+            </header>
             <main className='p-12'>
                 {children}
             </main>
