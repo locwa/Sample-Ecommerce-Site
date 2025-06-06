2093794a7c0086d97f4eec29440ec4f21c7c4aa9
locwa <94048557+locwa@users.noreply.github.com>
2025-06-06 18:15:22 +0800

    Made header stick to the top

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -10,7 +10,7 @@ export default function Layout(
     return(
         <>
             <title>OmniShop</title>
-            <header>
+            <header className="sticky top-0 w-full h-full z-3">
                 <Navbar />
                 {isCartOpen && (
                         <>
