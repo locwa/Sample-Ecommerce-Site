74f6a716652d7efdcb366851bb9fb8324d1ebad3
locwa <94048557+locwa@users.noreply.github.com>
2025-06-11 20:36:00 +0800

    replaced code in api/index.php to just require index.php from public

--- backend/api/index.php
+++ backend/api/index.php
@@ -1,38 +1,4 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
-
-use Dotenv\Dotenv;
-
-$dotenv = Dotenv::createImmutable(__DIR__, '/../.env');
-$dotenv->safeLoad();
-
-header('Access-Control-Allow-Origin: '. $_ENV['FRONTEND_APP_URL']);
-header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
-header('Access-Control-Allow-Credentials: true');
-
-
-$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
-    $r->post('/graphql', [App\Controller\GraphQL::class, 'handle']);
-});
-
-$routeInfo = $dispatcher->dispatch(
-    $_SERVER['REQUEST_METHOD'],
-    $_SERVER['REQUEST_URI']
-);
-
-switch ($routeInfo[0]) {
-    case FastRoute\Dispatcher::NOT_FOUND:
-        // ... 404 Not Found
-        break;
-    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
-        $allowedMethods = $routeInfo[1];
-        // ... 405 Method Not Allowed
-        break;
-    case FastRoute\Dispatcher::FOUND:
-        $handler = $routeInfo[1];
-        $vars = $routeInfo[2];
-        echo $handler($vars);
-        break;
-}
+// Forward vercel requests to index.php in public directory
+require __DIR__ . '/../public/index.php';
