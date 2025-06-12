2695aa548e1e72410f29e67401a32fb066794d1b
locwa <94048557+locwa@users.noreply.github.com>
2025-06-12 10:43:18 +0800

    Replaced code in getProductGallery

--- backend/src/Models/Products/AllProducts.php
+++ backend/src/Models/Products/AllProducts.php
@@ -104,16 +104,23 @@ class AllProducts extends AbstractProducts
      */
 
     public static function getProductGallery(string $id) : array{
-        $db = new Database();
-        $stmt = $db->prepare("SELECT link FROM gallery WHERE product_id = ?");
-        $stmt->execute([$id]);
-        $res = $stmt->fetchAll(PDO::FETCH_OBJ);
-
-        $resultArray = [];
-
-        foreach($res as $gallery){
-            array_push($resultArray, $gallery->link);
+        if (empty($productIds)) {
+            return [];
         }
-        return $resultArray;
+
+        $db = new Database();
+        $inQuery = implode(',', array_fill(0, count($productIds), '?'));
+
+        $stmt = $db->prepare("SELECT product_id, image_url FROM product_gallery WHERE product_id IN ($inQuery)");
+        $stmt->execute($productIds);
+
+        $galleryData = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $grouped = [];
+        foreach ($galleryData as $row) {
+            $grouped[$row['product_id']][] = $row['image_url'];
+        }
+
+        return $grouped;
     }
 }
