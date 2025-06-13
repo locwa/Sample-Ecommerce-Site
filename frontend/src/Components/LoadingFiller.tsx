2d63b31836163aeab88b0a2093ed26fe214bbbe9
locwa <94048557+locwa@users.noreply.github.com>
2025-06-13 19:33:01 +0800

    Changed tag from h4 to p

--- frontend/src/Components/LoadingFiller.tsx
+++ frontend/src/Components/LoadingFiller.tsx
@@ -4,7 +4,7 @@ export function LoadingFiller() {
         <div className="h-[70vh] flex flex-col justify-center">
             <div className="flex flex-col items-center gap-y-5">
                 <LoadingSpinner />
-                <h4>Loading</h4>
+                <p>Loading</p>
             </div>
         </div>
     )
