b87e47260f3b5dbd475819432043916937c69fbb
locwa <94048557+locwa@users.noreply.github.com>
2025-06-14 00:28:43 +0800

    Added data-testid to swatch

--- frontend/src/Components/AttributeSelector.tsx
+++ frontend/src/Components/AttributeSelector.tsx
@@ -17,7 +17,7 @@ export default function AttributeSelector({id, type, itemValue, selectedId, onSe
     } else if (mode === "cart") {
         toggleStylingText = isSelected ? "bg-black text-white px-3 py-1" : "bg-gray-100 px-3 py-1";
         toggleStylingSwatch = isSelected ? "border border-[#5ECE7B] w-6 h-6" : "w-6 h-6";
-        dataTestId = isSelected ? `cart-item-attribute-${attributeName}-${itemValue}-selected`:  `cart-item-attribute-${attributeName}-${itemValue}`
+        dataTestId = isSelected ? `cart-item-attribute-${attributeName?.toLowerCase()}-${itemValue.toLowerCase()}-selected`:  `cart-item-attribute-${attributeName?.toLowerCase()}-${itemValue.toLowerCase()}`
         colorSwatch = "w-5 h-5 p-1 border border-black";
     }
 
@@ -36,6 +36,9 @@ export default function AttributeSelector({id, type, itemValue, selectedId, onSe
         <button
             key={id}
             className={"flex items-center justify-center " + toggleStylingSwatch}
+            {...(mode === "cart" && {
+                [`data-testid`]: dataTestId,
+            })}
             onClick={() => onSelect(id)}
         >
             <div
