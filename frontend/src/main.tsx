768813d6a6e72a0b9f3d7db335f37d61f8cff771
locwa <94048557+locwa@users.noreply.github.com>
2025-06-13 23:47:00 +0800

    Added fonts

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './fonts.css'
 import './index.css'
 import ProductList from './Pages/ProductList.tsx'
 import ProductDetails from "./Pages/ProductDetails.tsx";
