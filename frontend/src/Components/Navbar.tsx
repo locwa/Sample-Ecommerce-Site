3204368a67495ab762b59c99da3a3f91c18b4807
locwa <94048557+locwa@users.noreply.github.com>
2025-06-14 13:05:27 +0800

    added "all" path and data-testid

--- frontend/src/Components/Navbar.tsx
+++ frontend/src/Components/Navbar.tsx
@@ -16,12 +16,12 @@ function NavlinkComponent(
     if (isActive) {
         return (
             <div className='border-b border-[#5ECE7B] h-12 px-2 flex items-center '>
-                <NavLink to={ path } className='font-bold text-[#5ECE7B]'>{name}</NavLink>
+                <NavLink to={ path } className='font-bold text-[#5ECE7B]' data-testid="active-category-link">{name}</NavLink>
             </div>
         )
     } else {
         return (
-            <NavLink to={ path } className='h-12 px-2 flex items-center'>{name}</NavLink>
+            <NavLink to={ path } className='h-12 px-2 flex items-center' data-testid="category-link">{name}</NavLink>
         )
     }
 }
@@ -33,21 +33,18 @@ export default function Navbar() {
         <nav className='sticky top-0 z-50 bg-white w-full flex justify-between items-center px-10 py-2 bg-white z-2' onClick={ () => isCartOpen && closeCart()}>
             <div className='flex gap-10' >
                 <NavlinkComponent
-                    path='/'
-                    isActive={(category == undefined)}
-                    data-testid={category == undefined ? "category-link" : "active-category-link"}
+                    path='/all'
+                    isActive={(category == undefined || category == "all")}
                     name='ALL'
                 />
                 <NavlinkComponent
                     path='/clothes'
                     isActive={category == "clothes"}
-                    data-testid={category == "clothes" ? "category-link" : "active-category-link"}
                     name='CLOTHES'
                 />
                 <NavlinkComponent
                     path='/tech'
                     isActive={category == "tech"}
-                    data-testid={category == "tech" ? "category-link" : "active-category-link"}
                     name='TECH'
                 />
             </div>
